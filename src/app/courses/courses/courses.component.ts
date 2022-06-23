ded90dda599865bdec1ce2bab57d96c5d2af2d4c
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-22 21:40:39 -0300

    criando o service

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Course } from './../model/course';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -9,14 +10,15 @@ import { Course } from './../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'Frontend' },
-    { _id: '2', name: 'React', category: 'Frontend' },
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  // coursesService: CoursesService
+
+  constructor(private coursesService: CoursesService) {
   //  this.courses = [];
+  // this.coursesService = new CoursesService();
+  this.courses = this.coursesService.list();
   };
 
   ngOnInit(): void { };
