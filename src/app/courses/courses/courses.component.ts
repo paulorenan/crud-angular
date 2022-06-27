ace4c4cfffca495fd5b11986c684c883097af1d6
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-27 11:09:29 -0300

    dialog error

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
 
 import { Course } from './../model/course';
 import { CoursesService } from './../services/courses.service';
@@ -16,11 +18,26 @@ export class CoursesComponent implements OnInit {
 
   // coursesService: CoursesService
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     //  this.courses = [];
     // this.coursesService = new CoursesService();
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Error ao carregar cursos')
+        return of([]);
+      })
+    )
   };
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
 
   ngOnInit(): void { };
 
